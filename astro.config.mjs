556904be53d26812f09b1065b414bc99182c3393
Crspy <crspy8687@gmail.com>
2026-04-27 11:15:23 -0700

    Set up scroll-based multi-route layout with sticky SectionBar
    Setup separate pages for sections on main page, and utilizing GSAP scroll plugins to handle route transitioning through page scrolling. Created a sticky bar that is visible in each page or section.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
-import {defineConfig, fontProviders} from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config'
+
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://crspy.me",
+
+  fonts: [
+      {
+          name: "Playfair Display",
+          cssVariable: "--font-playfair",
+          provider: fontProviders.google(),
+          weights: [400, 700],
+      },
+      {
+          name: "Inter",
+          cssVariable: "--font-inter",
+          provider: fontProviders.google(),
+          weights: [300, 400, 500, 700],
+      },
+  ],
+
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});

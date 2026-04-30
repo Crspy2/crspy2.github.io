0fbe7578678ea23dd829abbe1650ea69914a7ece
Crspy <crspy8687@gmail.com>
2026-04-29 19:09:19 -0700

    Scaffold MDX blog content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, fontProviders } from 'astro/config'
 
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://crspy.me",
@@ -29,5 +31,5 @@ export default defineConfig({
       },
   ],
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
 });
